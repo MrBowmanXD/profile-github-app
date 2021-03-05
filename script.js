76ca67b2d51e4c83f347bd4155c3924497ef4e9f
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2021-03-05 18:18:41 +0000

    Project version 1.0.1

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Get the Github username input form
-// const gitHubForm = document.getElementById("gitHubForm");
-
-
-
 const generate = document.querySelector(".generate");
 
 // Listen to click event to generate html
@@ -24,21 +19,57 @@ generate.addEventListener("click", function showData (e) {
 
 function requestUserRepos(username) {
 
+    if (!username) return;
+
+    clear();
+
     const containerDisplay = document.querySelector(".containerDisplay");
 
     // Create a new XMLHttpRequest
     const xhr = new XMLHttpRequest();
 
+    const request = new XMLHttpRequest();
+
     // Github endpoint, dinamically passing in specified username
     const url = `https://api.github.com/users/${username}/repos`;
+
+    const URL = `https://api.github.com/users/${username}`;
+
+    
 
     // Open a new connection, using a GET request via URL endpoint
     // Providing 3 arguments (GET/POST, The URL, Async True/False)
     xhr.open("GET", url, true);
 
+    request.open("GET", URL, true);
+
+    request.onload = function() {
+
+        const data = JSON.parse(this.response);
+
+        
+
+        let html = `<br>
+        <div class="TopDisplay">
+            <h1 class="usernameDisplay">${data.name}</h1>
+            <p class="website"><b>website:</b></p><a href="https://github.com/${username}"> https://github.com/${username}</a>
+            <br>
+            <hr>
+            <br>
+        </div>`;
+
+        containerDisplay.innerHTML += html;
+
+    }
+
+    request.send();
+
     // When request is received 
     // Process it here
     xhr.onload = function() {
+
+        
+
 
         // Parse API data into JSON 
         const data = JSON.parse(this.response);
@@ -48,28 +79,27 @@ function requestUserRepos(username) {
 
             const containerDisplay = document.querySelector(".containerDisplay");
 
+            console.log(data[i].languages_url);
+
             // Add html to container div
-            let html = ` <div class="TopDisplay">
-            <h1 class="usernameDisplay">${username}</h1>
-            <p class="website">${data[i].url}</p>
-        <br>
-        <hr>
-        <br>
-    </div>
+            let html = `
+            
     <div class="repositories">
-        <h2 class="h2Repositories">Repositories</h2>
-            <p>${data[i].name}</p>
-            <p>${data[i].html_url}</p>
-            <p>${data[i].description}</p>
+        
+        <h2 class="h2Repositories">Repository</h2>
+        
+            <p><b>name:</b></p><p> ${data[i].name}</p>
+            <p><b>url:</b></p><a href="${data[i].html_url}"> ${data[i].html_url}</a>
+            <p><b>description:</b></p><p> ${data[i].description}</p>
         <br>
         <hr>
         <br>
     </div>
     <div class="languages">
-        <h2 class="h2Languages">Languages</h2>
+        <h2 class="h2Languages">Repo-Language</h2>
         <p>${data[i].language}</p>
         <br>
-        
+        <hr>
     </div>`
 
             containerDisplay.innerHTML += html;
@@ -82,3 +112,6 @@ function requestUserRepos(username) {
 
 }
 
+function clear () {
+    containerDisplay.innerHTML = "";
+}
