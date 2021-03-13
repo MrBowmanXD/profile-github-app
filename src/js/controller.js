5edbc4e1bf1f309db86d393e4b02700a30a8f5e1
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2021-03-13 18:49:32 +0000

    Add files via upload

---
+++ src/js/controller.js
@@ -0,0 +1,12 @@
+import { containerDisplay, generate, usernameInput, gitHubUsername } from "./config.js";
+import { requestUserRepos } from "./model.js";
+
+// Listen to click event to generate html
+generate.addEventListener("click", function showData (e) {
+
+    // Prevent default button submission action
+    e.preventDefault();
+
+    // Run GitHub API function, passing in the GitHub username
+    requestUserRepos(gitHubUsername);
+});
